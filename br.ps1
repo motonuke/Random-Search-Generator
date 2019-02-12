52eebc7a320c61282d250d2a170d682e58822a80
motonuke <twnukowski@gmail.com>
2019-02-12 09:48:14 -0500

    Update br.ps1
    Initial import and cleanup edit

--- br.ps1
+++ br.ps1
@@ -31,22 +31,3 @@ write-output "All done with this round..."
 get-date
 start-sleep 10
 start-process microsoft-edge:https://www.bing.com/rewards/dashboard
-
-
-### Old code kept down here for reference
-
-# function random_wl {$script:random_n1 = get-random -input (1..$words.length) -Count 5}
-
-	# $get_word = Invoke-WebRequest -Uri https://randomword.com
-	# $random_word = $get_word.AllElements | where {$_.innerhtml -ilike "*random_word*"} | select -ExpandProperty InnerText -last 1
-	# $random_word = $random_word.Split( )
-	# $random_word = $random_word.Replace(";","")
-	
-	
-		# $random_time = get-random -minimum 60 -maximum 300
-	# $useragent = "User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)"
-	# $wc6 = (new-object System.net.WebClient)
-	# $wc6.Headers.Add("user-agent", $userAgent)
-	# $ie = New-Object -ComObject "InternetExplorer.Application"
-	# $ie.visible = $true
-	# $ie.navigate("http://www.bing.com/search?q=$phrase")
